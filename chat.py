c284f2fb52ba690f9a79f39d93d376f07199d31d
GONNIM <suauncle@gmail.com>
2024-07-16 19:06:44 +0900

    Modified Tax Chatbot App - chat.py > title, llm.py > system_prompt

--- chat.py
+++ chat.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 from llm import get_ai_response
     
-st.set_page_config(page_title="소득세 챗봇", page_icon="🤖")
+st.set_page_config(page_title="소득세법 챗봇", page_icon="🤖")
 
-st.title("🤖 소득세 챗봇")
+st.title("🤖 소득세법 챗봇")
 st.caption("소득세에 관한 모든 것을 답변해 드립니다.")
 
 load_dotenv()
@@ -18,7 +18,7 @@ for message in st.session_state.message_list:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 내용들을 말씀해주세요!"):
+if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 내용들을 말씀해 주세요!"):
     with st.chat_message("user"):
         st.write(user_question)
     st.session_state.message_list.append({"role": "user", "content": user_question})
